5b6c392456c7f432e6ff78444005080cbd7283f2
Robbuss <vincentvanmontfoort@gmail.com>
2018-06-23 12:54:40 +0200

    small refactor #2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,24 +8,24 @@ $request = Request::createFromGlobals();
 
 $number = $request->get('number');
 
-if(is_numeric($number)){
-    if($number > 999999999){
+if (is_numeric($number)) {
+    if ($number > 999999999) {
         $responseString = 'Whoaa easy there cowboy! What do you think I am? <br>Some kind of Supercomputer? Try a small number or I get overheated!';
-    }else{
+    } else {
         $prime = new Prime();
         $primeFactors = $prime->generate($number);
         $primeNumber = $prime->reverse($primeFactors);
         $isPrime = $prime->isPrime($primeFactors);
-        
-        if($isPrime){
-            $responseString = $primeNumber .' is not a prime number. <br>';
-            $responseString .= 'Factors: '.implode(" * ", $primeFactors) . ' = ' . $primeNumber;
-        }else{
+
+        if ($isPrime) {
+            $responseString = $primeNumber . ' is not a prime number. <br>';
+            $responseString .= 'Factors: ' . implode(" * ", $primeFactors) . ' = ' . $primeNumber;
+        } else {
             $responseString = $primeNumber . ' is definitely a prime number<br>';
         }
     }
-}else{
+} else {
     $responseString = 'Not a number';
 }
 
-include('form.php');
+include 'form.php';
